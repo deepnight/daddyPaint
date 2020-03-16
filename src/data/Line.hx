cfd7471fec8571d6822e5e24268719ff0907a98e
Sébastien Bénard <github@deepnight.net>
2020-03-16 11:13:38 +0100

    Added Line constructor

--- src/data/Line.hx
+++ src/data/Line.hx
@@ -6,4 +6,14 @@ class Line {
 	public var tx : Int;
 	public var ty : Int;
 	public var color : UInt;
+
+	public function new(fx:Float, fy:Float, tx:Float, ty:Float, c:UInt) {
+		this.fx = Std.int(fx);
+		this.fy = Std.int(fy);
+
+		this.tx = Std.int(tx);
+		this.ty = Std.int(ty);
+
+		this.color = c;
+	}
 }
