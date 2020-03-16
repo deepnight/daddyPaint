713e51db3c2a16300cbbc9a79dee01ba22e9e16e
Sébastien Bénard <github@deepnight.net>
2020-03-16 11:20:14 +0100

    Basic drawing

--- src/Client.hx
+++ src/Client.hx
@@ -8,6 +8,13 @@ class Client extends Process {
 	public var fx : Fx;
 	public var hud : ui.Hud;
 
+	var drawing = false;
+	var color : UInt = 0xffffff;
+	// public var canvas : hxd.Pixels;
+	var lines : Array<Line> = [];
+	var lastPoint : h2d.col.Point;
+	var canvas : h2d.Graphics;
+
 	public function new() {
 		super(Main.ME);
 		ME = this;
@@ -18,6 +25,44 @@ class Client extends Process {
 
 		fx = new Fx();
 		hud = new ui.Hud();
+		lastPoint = new h2d.col.Point();
+		canvas = new h2d.Graphics(root);
+
+		Boot.ME.s2d.addEventListener( onEvent );
+	}
+
+	function onEvent(e:hxd.Event) {
+		switch e.kind {
+			case EPush:
+				startDrawing();
+
+			case ERelease, EReleaseOutside, EFocusLost:
+				stopDrawing();
+
+			case EMove:
+			case EOver:
+			case EOut:
+			case EWheel:
+			case EFocus:
+			case EKeyDown:
+			case EKeyUp:
+			case ETextInput:
+			case ECheck:
+		}
+	}
+
+	inline function getGlobalMouseX() return Boot.ME.s2d.mouseX;
+	inline function getGlobalMouseY() return Boot.ME.s2d.mouseY;
+
+	inline function getClientMouseX() return Std.int( getGlobalMouseX() / Const.SCALE );
+	inline function getClientMouseY() return Std.int( getGlobalMouseY() / Const.SCALE );
+
+	function startDrawing() {
+		drawing = true;
+		lastPoint.set( getClientMouseX(), getClientMouseY() );
+	}
+	function stopDrawing() {
+		drawing = false;
 	}
 
 	public function onCdbReload() {
@@ -45,6 +90,19 @@ class Client extends Process {
 
 	override function update() {
 		super.update();
+
+		if( drawing ) {
+			var mx = getClientMouseX();
+			var my = getClientMouseY();
+			if( mx!=lastPoint.x || my!=lastPoint.y ) {
+				canvas.lineStyle(8,color);
+				canvas.moveTo(lastPoint.x, lastPoint.y);
+				canvas.lineTo(mx,my);
+
+				lines.push( new data.Line(lastPoint.x, lastPoint.y, mx, my, color) );
+				lastPoint.set(mx,my);
+			}
+		}
 
 		if( !ui.Console.ME.isActive() && !ui.Modal.hasAny() ) {
 			#if hl
