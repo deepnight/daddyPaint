5cef95b2b634489c4d6a12e8e70172c04071d853
Sébastien Bénard <github@deepnight.net>
2020-03-16 14:26:52 +0100

    Added angle/length to Line

--- src/data/Line.hx
+++ src/data/Line.hx
@@ -7,6 +7,9 @@ class Line {
 	public var ty : Int;
 	public var color : UInt;
 
+	public var angle(get,never) : Float; inline function get_angle() return Math.atan2(ty-fy, tx-fx);
+	public var length(get,never) : Float; inline function get_length() return M.dist(fx,fy, tx,ty);
+
 	public function new(fx:Float, fy:Float, tx:Float, ty:Float, c:UInt) {
 		this.fx = Std.int(fx);
 		this.fy = Std.int(fy);
