e11241c81193e21dd8f15e310b984712bf2f4ea5
Sébastien Bénard <github@deepnight.net>
2020-03-18 13:14:46 +0100

    Brush size is more consistent

--- src/TouchDrawingData.hx
+++ src/TouchDrawingData.hx
@@ -27,7 +27,7 @@ class TouchDrawingData {
 	}
 
 	public function getBrushSize() {
-		return client.isEraser() ? 40 : client.baseBrushSize * ( 0.6 + 0.4*Math.cos(getElapsedTimeS()*8) );
+		return client.isEraser() ? 40 : client.baseBrushSize * ( 0.8 + 0.2*Math.cos(getElapsedTimeS()*8) );
 	}
 
 	public inline function getElapsedTimeS() return haxe.Timer.stamp() - startTime;
